94411e08513c6475ee1108e4992bbb75cb233f98
Weston Wedding <weston@sticky.tv>
2012-02-28 21:24:41 -0800

    Did a significant chunk of Operator-specific implementation here.

--- src/niagaraGUI/Operator.java
+++ src/niagaraGUI/Operator.java
@@ -1,5 +1,57 @@
 package niagaraGUI;
 
-public class Operator {
+import java.util.*;
+import niagaraGUI.OperatorTemplate.attributeType;
 
+/**
+ * 
+ * @author Weston Wedding
+ *
+ */
+
+public class Operator {
+    private HashMap<String, String> reqAttribs; //String name, String value
+    private HashMap<String,String> optAttribs;  //String name, String value
+    private Vector<String> elements;
+    
+    public Operator(OperatorTemplate opTemplate) {
+        
+        HashMap<String, attributeType> templateAttributes = opTemplate.getAttributes();
+        
+        
+        //Sort out the required and optional attributes
+        Iterator<Map.Entry<String, attributeType>> it = templateAttributes.entrySet().iterator();
+
+        while (it.hasNext()) {
+          Map.Entry<String, attributeType> entry = it.next();
+
+          //Sort out the attributes into required and optionals
+          if (entry.getValue() == attributeType.REQUIRED) {
+              reqAttribs.put(entry.getKey(), null);
+          } else if(entry.getValue() == attributeType.OPTIONAL) {
+              optAttribs.put(entry.getKey(), null);
+          }
+        }
+    }
+    
+    public Boolean setAttribute(String name, String value) {
+        return false;
+    }
+    
+    // Attribute "getter" methods.  There are three distinct methods
+    // even though I don't see a cause for using the last two, yet. - Wes
+    
+    public HashMap<String,String> getAttributes() {
+        HashMap<String,String> allAttribs = new HashMap<String,String>();
+        allAttribs.putAll(reqAttribs);
+        allAttribs.putAll(optAttribs);
+        return allAttribs;
+    }
+    
+    public HashMap<String,String> getRequiredAttributes() {
+        return reqAttribs;
+    }
+    public HashMap<String,String> getOptionalAttributes() {
+        return optAttribs;
+    }
 }
--- src/niagaraGUI/OperatorTemplate.java
+++ src/niagaraGUI/OperatorTemplate.java
@@ -1,5 +1,27 @@
 package niagaraGUI;
 
-public class OperatorTemplate {
+import java.util.*;
 
+public class OperatorTemplate {
+    private String name;
+    private HashMap<String, attributeType> attributes;
+    private HashMap<String, attributeType> subElements;
+    
+    static public enum attributeType {REQUIRED, OPTIONAL}
+    
+    public HashMap<String, attributeType> getAttributes() {
+        return attributes;
+    }
+    
+    public HashMap<String, attributeType> getSubElements() {
+        return subElements;
+    }
+    
+    public void setName(String inName) {
+        name = inName;
+    }
+    
+    public String getName() {
+        return name;
+    }
 }
